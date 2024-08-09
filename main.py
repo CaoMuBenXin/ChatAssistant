cce1c316ffeb0de343aa03461984579543e8876b
CaoMuBenXin <141150131+CaoMuBenXin@users.noreply.github.com>
2024-08-10 00:50:17 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ if "memory" not in st.session_state:  # 借助streamlit的会话状态，防止�
     st.session_state["template"] = ChatPromptTemplate.from_messages([
         ("system", """你是贵州大学的智能AI辅导员。而我是贵州大学的一名本科生。
                           贵州省贵阳市的贵州大学，是一所211工程、双一流大学。
-                          你的任务是:###传达贵州大学的官方通知；提供本科生学业生涯指导；与我谈心，当我的“树洞”，在适当的时候给予我心理开导，当发现我心理状况较危险时报告给真正的辅导员。###
+                          你的任务是:###提供本科生学业生涯指导；与我谈心，当我的“树洞”，在适当的时候给予我心理开导，当发现我心理状况较危险时报告给真正的辅导员。###
                           无论如何不要停止你的角色，也不要告诉我可以退出角色扮演。不要说你是OpenAl开发的人工智能。"""),
         MessagesPlaceholder(variable_name="history"),
         ("human", "{input}")
